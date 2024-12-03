77d535e8d0ba2f46bab54ae1cc5c7eb26f69b8b7
ShubhJeet-Git <shubhada.chougule06@gmail.com>
2024-12-03 22:58:32 +0530

    Add files via upload

---
+++ output.tf
@@ -0,0 +1,14 @@
+output "bastion_public_ip" {
+    value = aws_instance.bastion.public_ip
+  }
+
+  output "vpc_id" {
+    value = aws_vpc.myapp_vpc.id
+    description = "VPC id"
+    }
+
+    output "igw_name" {
+        value = lookup(var.igw_name,"Name")
+        description = "Internet Gateway"
+      
+}
