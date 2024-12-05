beef27803e953432d1f3528bd74b60a1bdb2acb3
ShubhJeet-Git <shubhada.chougule06@gmail.com>
2024-12-05 12:39:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.78.0"
     }
   }
+
+backend "s3" {
+    bucket         = "tfstate-save-bucket"
+    key            = "dev/dev-terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tfstate-Locktable" 
+  }
 }
 
 
